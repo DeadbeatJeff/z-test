9fa5f7ed744989a98d6b7d988941e853283782a0
Jeff Rolland <rollandj@uwm.edu>
2024-07-21 14:23:41 -0400

    Yet more testing.

--- z-text.R
+++ z-text.R
@@ -1,6 +1,6 @@
 z.test <- function (x, y = NULL, sigma1, sigma2 = NULL, alternative = "two-sided", mu = 0) 
 {
-  if (missing(x)) # Test again
+  if (missing(x)) # Test again and again
   {
     stop("You must specify a data column for x")
   }
